f512db9e2699464e95988c68555212a6e0f609cc
Junho Park <somedayif@hanyang.ac.kr>
2021-02-02 18:31:45 +0900

    Version 21.02.02.01
    remove the eclipse setting files

--- Release/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -lnative -lxenomai -lrtdm -lpthread -lPocoNet -lPocoFoundation -lethercat_rtdm -lNRMKHelperi686 -lNRMKhw_tp -lNRMKCore
-
